9d3da1f391bd5e3d995e0f209f2cdab4c1554b47
Marcel M. Cary <marcel@oak.homeunix.org>
2015-09-14 14:21:33 -0700

    Link to reports scaffolding from home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reports
   root to: "pages#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -6,4 +6,10 @@ describe "Home Page" do
     expect(page).to have_content "Valley Fire"
     expect(page).to have_content "Lost and Found"
   end
+
+  it "accepts reports" do
+    visit "/"
+    click_link "Report"
+    expect(current_path).to eq "/reports/new"
+  end
 end
