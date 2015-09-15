7efd25a288b0051a610d7c209a5a6dd4debbe828
Marcel M. Cary <marcel@oak.homeunix.org>
2015-09-15 05:48:44 -0700

    Add validations for presence and length

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,6 @@
 class Report < ActiveRecord::Base
+  validates :report_type, :summary, :reporter_name, :reporter_contact_info,
+    presence: true
+
+  validates :summary, length: { maximum: 40 }
 end
--- spec/features/report_spec.rb
+++ spec/features/report_spec.rb
@@ -15,4 +15,23 @@ describe "Reports" do
     expect(page).to have_content "Cat, Cinimon Tortoise Shell"
     expect(Report.first.report_type).to eq "found"
   end
+
+  it "requires fields" do
+    visit "reports/new"
+    # Fill in nothing.
+    click_button "Create Report"
+    error_heading = "4 errors prohibited this report from being saved"
+    expect(page).to have_content error_heading
+    expect(page).to have_content "Report type can't be blank"
+    expect(page).to have_content "Summary can't be blank"
+    expect(page).to have_content "Reporter name can't be blank"
+    expect(page).to have_content "Reporter contact info can't be blank"
+  end
+
+  it "caps summary length" do
+    visit "reports/new"
+    fill_in "Summary", with: "a" * 41
+    click_button "Create Report"
+    expect(page).to have_content "Summary is too long"
+  end
 end
