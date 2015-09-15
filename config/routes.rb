e176833366b221ccb48eb4e109325a2b9e218b56
Marcel M. Cary <marcel@oak.homeunix.org>
2015-09-14 22:02:37 -0700

    Add a proper About page with contact info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :reports
   root to: "pages#index"
+  get "/about", to: "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
