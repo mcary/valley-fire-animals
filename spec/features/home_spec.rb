be0d5c2cae7064be3b67edaad34c8bb831681345
Marcel M. Cary <marcel@oak.homeunix.org>
2015-09-15 06:00:11 -0700

    Fix broken specs
    The text used by these specs evolved and I haven't been running them
    diligently.  After getting a nil error last night, I'll be a little more
    diligent about that.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,12 +4,12 @@ describe "Home Page" do
   it "renders" do
     visit "/"
     expect(page).to have_content "Valley Fire"
-    expect(page).to have_content "Lost and Found"
+    expect(page).to have_content "Lost & Found"
   end
 
   it "accepts reports" do
     visit "/"
-    click_link "Report"
+    click_link "Report an Animal"
     expect(current_path).to eq "/reports/new"
   end
 end
