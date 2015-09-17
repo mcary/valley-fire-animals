b6ca70a52316a7e115fc942b8e05c3b6bb5384f7
Caleb Adam Haye <chaye@oreilly.com>
2015-09-17 15:59:56 -0700

    wip:better tests for foster home/animal

--- spec/features/foster_homes_spec.rb
+++ spec/features/foster_homes_spec.rb
@@ -6,10 +6,23 @@ require 'rails_helper'
 
 describe "Foster Homes" do
   
+  before do
+    create_animal "dog"
+    create_animal "cat"
+    create_animal "horse"
+  end
+  
   it "new" do
-    visit "foster_homes/new"
-    save_and_open_page
-    expect(page).to have_content "Cat, Cinimon Tortoise Shell"
+    visit "/foster_homes/new"
+    name = "Foo House"
+    address = "123 Bar St. Sebastopol, Ca. 9547"
+    fill_in "foster_home_name", with: name
+    fill_in "foster_home_address", with: address
+    check "foster_home_animal_cat"
+    click_on "Create Foster home"
+    expect(page).to have_content "Foster home was successfully created"
+    expect(page).to have_content "cat"
+    expect(page).not_to have_content "dog"
   end
   
   it "index" do
@@ -26,4 +39,13 @@ describe "Foster Homes" do
   end
   # it "edit" do
   # end
+
+  private
+
+  def create_animal(name="Dog")
+    visit "/animals/new"
+    fill_in "animal_name", with: name
+    click_on "Create Animal"
+    expect(page).to have_content "Animal was successfully created"
+  end
 end
